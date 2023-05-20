9faa08b86659ed0ddc6284ed80fa8d4018f32c30
suhettgabriel <suhettgabriel1983@gmail.com>
2023-05-20 17:10:12 -0300

    instalando e configurando o Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World")
+import dotenv  from 'dotenv';
+import express,{Express, Request, Response} from 'express';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json())
+
+app.get('/',(req: Request, res: Response) => {
+    res.send('Hello Word!');
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
